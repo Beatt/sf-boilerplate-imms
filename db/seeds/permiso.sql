b44f921973e89ead98b8434ada6008813df4b310
Gabriel <gabriel@donadora.mx>
2020-03-22 23:26:43 -0600

    permiso seed

---
+++ db/seeds/permiso.sql
@@ -0,0 +1,16 @@
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Agregar solicitud', 'Agregar solicitud', 'ROLE_AGREGAR_SOLICITUD');
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Detalle de solicitud', 'Detalle de solicitud', 'ROLE_DETALLE_SOLICITUD');
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Editar solicitud', 'Edición de solicitud', 'ROLE_EDITAR_SOLICITUD');
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Validar montos', 'Validación de montos', 'ROLE_VALIDACION_MONTOS');
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Editar montos', 'Edición de montos', 'ROLE_EDITAR_MONTOS');
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Inicio val info', 'Inicio de validación de información', 'ROLE_EDITAR_EI_DETALLE');
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Generar formatos', 'Generación de formatos', 'ROLE_GENERAR_FORMATO_PAGO');
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Cargar comprobante', 'Cargar comprobante', 'ROLE_CARGAR_COMPROBANTE_PAGO');
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Corrección de pago', 'Corregir comprobante de pago', 'ROLE_EDITAR_COMPROBANTE_PAGO');
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Consultar comprobante de pago', 'Consultar el comprobante de pago', 'ROLE_CONSULTAR_COMPROBANTE_PAGO');
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Validación de comprobante de pago', 'Validar el comprobante de pago', 'ROLE_VALIDAR_COMPROBANTE_PAGO');
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Consultar detalle IE', 'Consultar detalle de EI', 'ROLE_CONSULTAR_EI_DETALLE');
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Consultar factura', 'Consultar factura', 'ROLE_CONSULTAR_FACTURA');
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Cargar factura', 'Cargar factura', 'ROLE_CARGAR_FACTURA');
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Registro de montos', 'Registro de montos', 'ROLE_REGISTRO_MONTOS');
+INSERT INTO permiso (nombre, descripcion, rol_seguridad) VALUES ('Inicio', 'Seguimiento consultar', 'ROLE_CONSULTAR_SEGUIMIENTO');
