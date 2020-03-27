b9194bc80c7466fe9808fdd588052d6d7eee15eb
Gabriel Jiménez <beat_@comunidad.unam.mx>
2020-03-27 17:06:32 -0600

    Feature/translator (#4)
    Translate text with json type

---
+++ src/AppBundle/DTO/TraductorDTO.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\DTO;
+
+class TraductorDTO
+{
+    private $bar;
+
+    private $balance;
+
+    private $active;
+
+    /**
+     * @return mixed
+     */
+    public function getBar()
+    {
+        return $this->bar;
+    }
+
+    /**
+     * @param mixed $bar
+     */
+    public function setBar($bar)
+    {
+        $this->bar = $bar;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+
+    /**
+     * @param mixed $balance
+     */
+    public function setBalance($balance)
+    {
+        $this->balance = $balance;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+
+    /**
+     * @param mixed $active
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+    }
+}
