5f1a9e7a256cced94fe3b3041edea7d62a1cc8c0
Gabriel <gabriel@donadora.mx>
2020-03-31 21:36:19 -0600

    uninstall webpacker symfony bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,6 @@ class AppKernel extends Kernel
             new EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Knp\DoctrineBehaviors\Bundle\DoctrineBehaviorsBundle(),
-            new Symfony\WebpackEncoreBundle\WebpackEncoreBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
