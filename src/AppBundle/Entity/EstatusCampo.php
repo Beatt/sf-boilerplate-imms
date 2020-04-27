045a609f928ce044b97f6a37b4d25612a5a0917f
Gabriel <gabriel@donadora.mx>
2020-04-26 19:53:37 -0500

    mapping estatus campo

--- src/AppBundle/Entity/EstatusCampo.php
+++ src/AppBundle/Entity/EstatusCampo.php
@@ -10,8 +10,17 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class EstatusCampo
 {
+    const SOLICITUD_CREADA = 'solicitud_creada';
     const SOLICITUD_CONFIRMADA = 'solicitud_confirmada';
     const SOLICITUD_NO_AUTORIZADA = 'solicitud_no_autorizada';
+    const EN_VALIDACION_DE_MONTOS = 'en_validacion_de_montos';
+    const MONTOS_VALIDADOS = 'montos_validados';
+    const FORMATOS_DE_PAGO_GENERADOS = 'formatos_de_pago_generados';
+    const EN_VALIDACION_POR_FOFOE = 'en_validacion_por_fofoe';
+    const PAGO_NO_VALIDO = 'pago_no_valido';
+    const PENDIENTE_CFDI_FOFOE = 'pendiente_cfdi_fofoe';
+    const PAGO_VALIDADO = 'pago_validado';
+    const CREDENCIALES_GENERADAS = 'credenciales_generadas';
 
     /**
      * @var int
