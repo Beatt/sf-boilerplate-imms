300ac178bb3fd8d1454984736ca1603fddd48ead
Gabriel <gabriel@donadora.mx>
2020-06-10 16:25:40 -0500

    separando js por rol

--- webpack.config.js
+++ webpack.config.js
@@ -7,16 +7,21 @@ Encore
   .setPublicPath('/build')
   .copyFiles( {from: './assets/images', to: 'images/[path][name].[ext]'})
   .addStyleEntry('layout', './assets/css/layout.scss')
+  // IE
   .addEntry('ie.perfil', './assets/js/institucion_educativa/Perfil/index.js')
   .addEntry('ie.detalle.solicitud', './assets/js/institucion_educativa/DetalleSolicitud/index.js')
-  .addEntry('detalle_solicitud_multiple', './assets/js/institucion_educativa/DetalleSolicitudMultiple/index.js')
   .addEntry('ie.registrar.montos', './assets/js/institucion_educativa/RegistrarMontos/index.js')
-  .addEntry('registrar-pagos', './assets/js/institucion_educativa/RegistrarPago/index.js')
   .addEntry('ie.inicio', './assets/js/institucion_educativa/Inicio/index.js')
+  .addEntry('ie.detalle.forma.pago', './assets/js/institucion_educativa/DetalleFormaPago/index.js')
+  .addEntry('detalle_solicitud_multiple', './assets/js/institucion_educativa/DetalleSolicitudMultiple/index.js')
+  .addEntry('registrar-pagos', './assets/js/institucion_educativa/RegistrarPago/index.js')
+  .addEntry('seleccionar_forma_pago', './assets/js/institucion_educativa/SeleccionarFormaPago/index.js')
+
+  // CAME
   .addEntry('came_solicitud_index', './assets/js/came/solicitud/index.js')
   .addEntry('pregrado_reporte', './assets/js/pregrado/reporte/index.js')
-  .addEntry('seleccionar_forma_pago', './assets/js/institucion_educativa/SeleccionarFormaPago/index.js')
-  .addEntry('ie.detalle.forma.pago', './assets/js/institucion_educativa/DetalleFormaPago/index.js')
+
+  // FOFOE
   .addStyleEntry('layout-formato-fofoe', './assets/css/formatos/fofoe/layout.scss')
   .enableSingleRuntimeChunk()
   .cleanupOutputBeforeBuild()
