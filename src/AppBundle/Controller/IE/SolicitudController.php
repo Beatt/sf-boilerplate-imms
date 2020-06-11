91299aa72de89949ef4a90239a738a2c2746ae13
Gabriel <gabriel@donadora.mx>
2020-06-11 17:51:39 -0500

    fix bugs de vista de registrar montos

--- src/AppBundle/Controller/IE/SolicitudController.php
+++ src/AppBundle/Controller/IE/SolicitudController.php
@@ -188,6 +188,7 @@ class SolicitudController extends DIEControllerController
                         ]
                     ]
                 ]),
+            'errors' => $this->getFormErrors($form)
         ]);
     }
 
