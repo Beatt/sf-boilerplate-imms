5262715f01c4863986f25d9c67a36783a6d17098
Gabriel Jiménez <beat_@comunidad.unam.mx>
2020-06-29 16:33:11 -0500

    Fix/ie perfil (#43)
    * rfc mínimo 12 y máximo 13
    
    * se agrego el campo extensión de la institución
    
    * se agrego el campo extensión al frontend

---
+++ src/AppBundle/Migrations/Version20200629192644.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20200629192644 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE institucion ADD extension VARCHAR(5) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE institucion DROP extension');
+    }
+}
