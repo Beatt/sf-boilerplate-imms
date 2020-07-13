fe39f3a3ed8a2b8859c54610c40ba0db4683e271
Gabriel <gabriel@donadora.mx>
2020-07-12 20:50:15 -0500

    flash messages

--- src/AppBundle/Controller/IE/ComprobantePagoController.php
+++ src/AppBundle/Controller/IE/ComprobantePagoController.php
@@ -3,12 +3,16 @@
 namespace AppBundle\Controller\IE;
 
 use AppBundle\Controller\DIEControllerController;
+use AppBundle\Entity\CampoClinico;
 use AppBundle\Entity\Institucion;
 use AppBundle\Entity\Pago;
+use AppBundle\Entity\Solicitud;
 use AppBundle\Form\Type\ComprobantePagoType\ComprobantePagoType;
+use AppBundle\Repository\CampoClinicoRepository;
 use AppBundle\Repository\PagoRepositoryInterface;
 use AppBundle\Security\Voter\SolicitudVoter;
 use AppBundle\Service\UploaderComprobantePagoInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,6 +22,13 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ComprobantePagoController extends DIEControllerController
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * @Route("/pagos/{id}/cargar-comprobante-de-pago", name="ie#cargar_comprobante_de_pago", methods={"POST"})
      * @param int $id
@@ -57,11 +68,92 @@ class ComprobantePagoController extends DIEControllerController
             $pago = $form->getData();
             $uploaderComprobantePago->update($pago);
 
-            $this->addFlash('success', '¡El comprobante se ha cargado correctamente!');
+            $this->addFlash(
+                'success',
+                $this->getSuccessFlashMessage($pago)
+            );
             return new RedirectResponse($request->headers->get('referer'));
         }
 
-        $this->addFlash('danger', '¡Lo sentimos! Ha ocurrido un problema al cargar tu comprobante.');
+        $this->addFlash(
+            'danger',
+            $this->getFailedFlashMessage($pago)
+        );
         return new RedirectResponse($request->headers->get('referer'));
+    }
+
+    /**
+     * @return string
+     */
+    private function getSuccessFlashMessage(Pago $pago)
+    {
+        $solicitud = $pago->getSolicitud();
+        if($solicitud->isPagoUnico()) {
+            return sprintf(
+                '¡El comprobante de la solicitud %s se ha cargado correctamente!',
+                $solicitud->getNoSolicitud()
+            );
+        }
+        elseif(!$solicitud->isPagoUnico()) {
+            $campoClinico = $this->getCampoClinico($solicitud, $pago->getReferenciaBancaria());
+
+            return sprintf(
+                '¡El comprobante del campo clínico %s se ha cargado correctamente!',
+                $campoClinico->getUnidad()->getNombre()
+            );
+        }
+
+        $this->setCriticalLogUpdateComprobantePago($solicitud);
+
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    private function getFailedFlashMessage(Pago $pago)
+    {
+        $solicitud = $pago->getSolicitud();
+        if($solicitud->isPagoUnico()) {
+            return sprintf(
+                '¡Lo sentimos! Ha ocurrido un problema al cargar el comprobante de pago para la solicitud %s',
+                $solicitud->getNoSolicitud()
+            );
+        }
+        elseif(!$solicitud->isPagoUnico()) {
+            $campoClinico = $this->getCampoClinico($solicitud, $pago->getReferenciaBancaria());
+
+            return sprintf(
+                '¡Lo sentimos! Ha ocurrido un problema al cargar el comprobante de pago del campo clínico %s',
+                $campoClinico->getUnidad()->getNombre()
+            );
+        }
+
+        $this->setCriticalLogUpdateComprobantePago($solicitud);
+
+        return '';
+    }
+
+    /**
+     * @param Solicitud $solicitud
+     * @param $referenciaBancaria
+     * @return CampoClinico
+     */
+    private function getCampoClinico(Solicitud $solicitud, $referenciaBancaria)
+    {
+        /** @var CampoClinico $campoClinico */
+        return $solicitud->getCamposClinicos()->matching(
+            CampoClinicoRepository::getCampoClinicoByReferenciaBancaria($referenciaBancaria)
+        )->first();
+    }
+
+    /**
+     * @param Solicitud $solicitud
+     */
+    private function setCriticalLogUpdateComprobantePago(Solicitud $solicitud)
+    {
+        $this->logger->critical(sprintf('Se esta tratando de cargar un comprobante de pago sin haber asignado el tipo de pago'), [
+            'id' => $solicitud->getId()
+        ]);
     }
 }
