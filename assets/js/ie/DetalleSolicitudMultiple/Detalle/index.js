bda0e3b0b988ac85e5ff457246033f383bf5f137
Gabriel <gabriel@donadora.mx>
2020-07-17 20:30:36 -0500

    cleanup

--- assets/js/ie/DetalleSolicitudMultiple/Detalle/index.js
+++ assets/js/ie/DetalleSolicitudMultiple/Detalle/index.js
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import GestionPagoModal from "../../components/GestionPagoModal";
-import {dateFormat} from "../../../utils";
 
 const DetalleSolicitudMultiple = ({ solicitud }) => {
   const { useState } = React
