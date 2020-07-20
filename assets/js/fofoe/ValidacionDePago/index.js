2cc131cfd77da1fcd26787918f2dcce4902d84a9
Gabriel <gabriel@donadora.mx>
2020-07-19 20:32:40 -0500

    mostrar información del pago y tabla de historial de pago

--- assets/js/fofoe/ValidacionDePago/index.js
+++ assets/js/fofoe/ValidacionDePago/index.js
@@ -3,8 +3,52 @@ import ReactDOM from 'react-dom';
 
 const ValidacionDePago = () => {
 
+  function isPagoMultiple() {
+    return true;
+  }
+
   return(
-    <h2>Componente de validación de pago</h2>
+    <div className='row mt-20'>
+      <div className="col-md-12">
+        <div className="row">
+          <div className="col-md-6">
+            <p className='mb-5'>No. de Solicitud <strong>Numero de solicitud</strong></p>
+            <p className='mb-5'>Tipo de pago <strong>Tipo de pago</strong></p>
+            <p className='mb-20'>Monto total: <strong>Monto total</strong></p>
+          </div>
+          {
+            isPagoMultiple() &&
+            <div className="col-md-6">
+              <p className='mb-5'><strong>Campo clínico</strong></p>
+              <p className='mb-5'>Sede: <strong>Sede</strong></p>
+              <p className='mb-5'>Carrera <strong>Carrera</strong></p>
+            </div>
+          }
+        </div>
+      </div>
+      <div className="col-md-12 mb-20">
+        <table className='table table-condensed'>
+          <thead>
+          <tr>
+            <th>No de referencia</th>
+            <th>Comprobante registrado</th>
+            <th>Fecha</th>
+            <th>Monto validado</th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td
+                className='text-center text-info'
+                colSpan={4}
+              >
+                Aún no se ha cargado ningún comprobante de pago
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
