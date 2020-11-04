14e3ed26e1cfdae2beb4ed2e726d3f6463d3bb5d
Oscar <oscarhconstantino@gmail.com>
2020-11-03 19:01:01 -0600

    Backend, validacion de descuentos CAME

--- src/AppBundle/Controller/Came/SolicitudController.php
+++ src/AppBundle/Controller/Came/SolicitudController.php
@@ -330,9 +330,21 @@ class SolicitudController extends DIEControllerController
         $form->get('montos_pagos')->setData($solicitud->getMontosCarreras());
         $form->handleRequest($request);
 
+        $originalDescuentos = array();
+        foreach ($solicitud->getMontosCarreras() as $monto) {
+            $originalDescuentos[$monto->getId()] = array();
+            foreach ($monto->getDescuentos() as $descuento) {
+                if ($descuento->getId())
+                    $originalDescuentos[$monto->getId()][$descuento->getId()] = $descuento;
+            }
+        }
+
         if ($form->isSubmitted() && $form->isValid()) {
             $result = $solicitudManager->validarMontos($form->getData(),
-                $form->get('montos_pagos')->getData(), isset($request->request->get('solicitud')['validado']), $this->getUser());
+                $form->get('montos_pagos')->getData(),
+                isset($request->request->get('solicitud')['validado']),
+                $this->getUser(), $originalDescuentos
+            );
             if ($result['status']) {
                 $this->addFlash('success', "Se ha procesado la validación de montos de la solicitud {$solicitud->getNoSolicitud()} con éxito");
             }
